e0512ed1777d1266c9b426cc01a11d5dd0ef6608
prajilprasad <147649749+prajilprasad@users.noreply.github.com>
2024-11-13 15:47:22 +0530

    Update .gitignore

---
+++ apibackendapp/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from .models import Department ,Employee
+from datetime import date
+from django.urls import reverse
+from rest_framework import status
+from  .serializers import EmployeeSerializer 
+# Create your tests here.
+class EmployeeViewSetTest(APITestCase):
+    def setUp(self):
+        self.departments = Department.objects.create(Department_name="HR")
+        self.employee = Employee.objects.create(
+            employee_name ="jakie",
+            designation ="",
+            date_of_joining = date(2024,11,13),
+            department = self.departments,
+            contact ="China",
+            is_active = True
+        )
+        self.client = APIClient()
+        
+    def test_employee_details(self):
+        url = reverse('employee-detail' , args={self.employee.employee_id})
+        response = self.client.get(url)
+        employee = Employee.objects.all()
+        serializer= EmployeeSerializer(self.employee)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer. data)
